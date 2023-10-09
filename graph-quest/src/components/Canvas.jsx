31a1e82a6e58123a3000c7ee614188c9165f2f6e
M Mire <mmire@Ms-MacBook-Air.local>
2023-10-09 18:14:48 +0100

    canvas now shows the nodes from the edit modes

--- graph-quest/src/components/Canvas.jsx
+++ graph-quest/src/components/Canvas.jsx
@@ -1,9 +1,15 @@
 import DraggableNav from "./DraggableNav";
 import Node from "./Node";
+import Edge from "./Edge";
 import EditSharpIcon from "@mui/icons-material/EditSharp";
 import ShuffleIcon from "@mui/icons-material/Shuffle";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
-import React, { useState, useCallback } from "react";
+import React, { useState, useRef, useEffect } from "react";
+
+const getCoords = (nodes, target) => {
+  const node = nodes.find((node) => node.val === target);
+  return { x: node.x, y: node.y };
+};
 
 export default function Canvas({ enableEditMode, nodes }) {
   const NAV_ITEMS = [
@@ -20,31 +26,53 @@ export default function Canvas({ enableEditMode, nodes }) {
     },
   ];
 
-  const [height, setHeight] = useState(0);
   const [isMouseDown, setMouseDown] = useState(false);
-  const measuredRef = useCallback((node) => {
-    if (node !== null) {
-      setHeight(node.getBoundingClientRect().height);
-    }
+  const [cavnasSize, setCanvasSize] = useState({});
+  const elementRef = useRef(null);
+  useEffect(() => {
+    setCanvasSize({
+      height: elementRef.current.clientHeight,
+      width: elementRef.current.clientWidth,
+    });
   }, []);
 
   return (
     <div
       id="canvas"
       className="canvas"
-      ref={measuredRef}
+      ref={elementRef}
       onMouseUp={() => {
         setMouseDown(false);
       }}>
       <DraggableNav
-        canvasHeight={height}
+        canvasHeight={cavnasSize.height}
         handleMousedown={() => setMouseDown(true)}
         ismousedown={isMouseDown}
         NAV_ITEMS={NAV_ITEMS}
       />
-      {nodes.map((node) => {
-        return <Node key={node.id} val={node.val} x={node.x} y={node.y} />;
-      })}
+      <svg
+        id="canvas-edit-mode"
+        style={{ height: cavnasSize.height, width: cavnasSize.width }}>
+        {nodes?.map((node) => {
+          return <Node key={node.id} val={node.val} x={node.x} y={node.y} />;
+        })}
+        {nodes
+          .filter((node) => node.childNodes.size > 0)
+          .map((node) => {
+            return Array.from(node.childNodes).map((child) => {
+              const childCoords = getCoords(nodes, child);
+              return (
+                <Edge
+                  key={"" + child.id + node.id}
+                  x1={node.x}
+                  y1={node.y}
+                  x2={childCoords.x}
+                  y2={childCoords.y}
+                />
+              );
+            });
+          })}
+      </svg>
     </div>
   );
 }

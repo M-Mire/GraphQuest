02666124df5ee3bc868f3d18b95614a7ccf2964b
M Mire <mmire@Ms-MacBook-Air.local>
2023-10-30 17:15:40 +0000

    adding bfs line highliting

--- src/app/_GraphAlgorithm/Graph.ts
+++ src/app/_GraphAlgorithm/Graph.ts
@@ -34,22 +34,42 @@ export default class Graph {
   }
 
   BFS(root: number) {
+    this.TRACKER.push([Line.EntryLine, 1]);
+    this.TRACKER.push([Line.EntryLine, 2]);
     const visited = new Set();
+    this.TRACKER.push([Line.FinishedLine, 2]);
+    this.TRACKER.push([Line.EntryLine, 3]);
     const queue = [root];
+    this.TRACKER.push([Line.FinishedLine, 3]);
     this.TRACKER.push([Command.Visited, root]);
+    this.TRACKER.push([Line.EntryLine, 4]);
     visited.add(root);
+    this.TRACKER.push([Line.FinishedLine, 4]);
     while (queue.length > 0) {
+      this.TRACKER.push([Line.EntryLine, 6]);
+      this.TRACKER.push([Line.EntryLine, 7]);
       const vertex = queue.shift();
+      this.TRACKER.push([Line.FinishedLine, 7]);
       this.TRACKER.push([Command.EnteredQueue, vertex]);
       for (const neighbour of this.graph.get(vertex) || []) {
+        this.TRACKER.push([Line.EntryLine, 8]);
         if (!visited.has(neighbour)) {
+          this.TRACKER.push([Line.EntryLine, 9]);
           this.TRACKER.push([Command.Visited, neighbour]);
+          this.TRACKER.push([Line.EntryLine, 10]);
           visited.add(neighbour);
+          this.TRACKER.push([Line.FinishedLine, 10]);
+          this.TRACKER.push([Line.EntryLine, 11]);
           queue.push(neighbour);
+          this.TRACKER.push([Line.FinishedLine, 11]);
+          this.TRACKER.push([Line.FinishedLine, 9]);
         }
+        this.TRACKER.push([Line.FinishedLine, 8]);
       }
       this.TRACKER.push([Command.PoppedQueue, vertex]);
+      this.TRACKER.push([Line.FinishedLine, 6]);
     }
+    this.TRACKER.push([Line.FinishedLine, 1]);
   }
 
   DFS(root: number) {
